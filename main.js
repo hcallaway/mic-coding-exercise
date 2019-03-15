2ca25cb9ddaa84440bc4b351be7d3fa774f4668c
Houston <houstoncallaway@gmail.com>
2019-03-15 00:18:05 -0500

    add comments to better explain thought process

--- main.js
+++ main.js
@@ -25,23 +25,27 @@ frequencySelector.addEventListener('change', (evt) => {
 // Event Listener for  Window Resize
 window.addEventListener('resize', onResize);
 
+// Grab page dimensions
 function updatePageDimensions() {
     pageWidth = window.innerWidth;
     pageHeight = window.innerHeight / 3;
 }
 
+// Manually call 1st time to have access to dimensions
 updatePageDimensions()
 
-
+// Initiate list of pixel points across page
 let xplotPoints = []
+// Grab each pixel point and add to array
 for (var i = 0; i <= pageWidth; i++) {
     xplotPoints.push(i)
 }
 
 let t = 0
 
+// Sine Wave Animation
 function animate() {
-    
+    // Establish Y coordinate for each x coordinate
     let plotPoints = xplotPoints.map(x => {
 
         let y = pageHeight + amplitude * Math.sin((x + t) / wavelength)
@@ -49,17 +53,21 @@ function animate() {
         return [x, y]
     })
     
+    // Build SVG path from coordinates
     let path = "M" + plotPoints.map(p => {
         return p[0] + "," + p[1]
     }).join(" L")
     
+    // Send SVG path to the DOM
     document.getElementById("wave").setAttribute("d", path)
     
     t += frequency
     
+    // Animation
     requestAnimationFrame(animate)
 }
 
+// Check for page resize (for establishing page dimensions)
 function onResize() {
     updatePageDimensions();
 };
